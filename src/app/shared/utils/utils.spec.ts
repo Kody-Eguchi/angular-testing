205dd8c8ea759c0ed00a58b85817d4bd3b0ff600
Koji Eguchi <koji.eguchi@hotmail.com>
2024-02-29 19:58:49 -0500

    Create utils.ts and utils.spec.ts

---
+++ src/app/shared/utils/utils.spec.ts
@@ -0,0 +1,13 @@
+describe('utils', () => {
+  describe('range', () => {
+    it('some name of the test', () => {
+      expect(1).toEqual(1);
+    })
+  })
+
+  describe('pluck', () => {
+    it('some name of the test', () => {
+      expect(1).toEqual(1);
+    })
+  })
+})
